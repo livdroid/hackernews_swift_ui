70735752d0a64ffa42a194a295534948c0cc667f
livdroid <oliviarodrigue@gmail.com>
2022-06-12 00:12:22 +0200

    Initial Commit

---
+++ hackernews/hackernewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackernewsApp.swift
+//  hackernews
+//
+//  Created by olivia on 12/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct hackernewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
